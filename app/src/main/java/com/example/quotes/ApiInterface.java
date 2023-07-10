7e8d9ea6a6e69251baf4cb3625fbc9bc14392bb4
KUSHAGRA SHARMA <kushagras652@gmail.com>
2023-07-10 23:50:28 +0530

    Retrofit Instance

--- app/src/main/java/com/example/quotes/ApiInterface.java
+++ app/src/main/java/com/example/quotes/ApiInterface.java
@@ -1,6 +1,12 @@
 package com.example.quotes;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface ApiInterface {
 
-
+    @GET("v1/quotes?category=happiness")
+    Call<List<userModel>> getUser();
 }
--- app/src/main/java/com/example/quotes/MainActivity.java
+++ app/src/main/java/com/example/quotes/MainActivity.java
@@ -3,14 +3,22 @@ package com.example.quotes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static String api="https://api-ninjas.com/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +30,17 @@ public class MainActivity extends AppCompatActivity {
         TextView next=findViewById(R.id.next);
         TextView previous=findViewById(R.id.previous);
 
-        String api="https://api.api-ninjas.com/v1/quotes?category=";
-        Retrofit retrofit=new Retrofit.Builder()
-                .baseUrl(api)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
+        RetrofitInstance.getInstance().apiInterface.getUser()
+                .enqueue(new Callback<List<userModel>>() {
+            @Override
+            public void onResponse(Call<List<userModel>> call, Response<List<userModel>> response) {
+                Log.d("god","onSuccess" + response.body().toString());
+            }
+
+            @Override
+            public void onFailure(Call<List<userModel>> call, Throwable t) {
+                Log.d("nidhish","onFailure" + t.getLocalizedMessage());
+            }
+        });
     }
 }
